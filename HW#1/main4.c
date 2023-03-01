123f15924e642224643c5a8efc40930a13da891b
Bellawang <51277857+Bella-w@users.noreply.github.com>
2023-03-01 21:41:05 +0800

    main4.c

---
+++ HW#1/main4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    char a[] = "-99acbc";
+    char b[] = "99acbc";
+    char c[] = "ac-99bc";
+    for(int i = 0 ; i < ((int)sizeof(a)/sizeof(char)-1) ; i++){
+        if((a[i] >= 48 && a[i] <= 57) || a[i] == 45){
+            printf("%c", a[i]);
+        }
+    }
+    printf("\n");
+    for(int i = 0 ; i < ((int)sizeof(b)/sizeof(char)-1) ; i++){
+        if((b[i] >= 48 && b[i] <= 57) || b[i] == 45){
+            printf("%c", b[i]);
+        }
+    }
+    printf("\n");
+    for(int i = 0 ; i < ((int)sizeof(c)/sizeof(char)-1) ; i++){
+        if((c[i] >= 48 && c[i] <= 57) || c[i] == 45){
+            printf("%c", c[i]);
+        }
+    }
+    printf("\n");
+    return 0;
+}

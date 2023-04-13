3b73783ee452828a3dcf6aab690ee3315a9be414
Bellawang <51277857+Bella-w@users.noreply.github.com>
2023-04-13 14:41:56 +0800

    Update main.c

--- Lab#6/main.c
+++ Lab#6/main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 void multi(int i,int j){
-
         if(i==9&&j==9){
             printf("%d*%d=%d\n",i,j,i*j);
             return ;
         }
+        else if(j==9){
+            printf("%d*%d=%d\n",i,j,i*j);
+            multi(i+1,1);
+        }
         else{
-            if(j==9){
-                printf("%d*%d=%d  ",i,j,i*j);
-                multi(i,j+1);
-            }
-            else if(i==9){
-                printf("%d*%d=%d  ",i,j,i*j);
-                multi(i+1,j);
-            }
-        } 
+            printf("%d*%d=%d  ",i,j,i*j);
+            multi(i,j+1);
+        }
        
 }
 int main()
